e3ad0be9248bcc49068c127b48867598d6a79aec
jiangying59143 <907292671@qq.com>
2019-10-09 20:27:10 +0800

    init version

---
+++ consumer/src/main/java/com/example/consumer/controller/ConsumerController.java
@@ -0,0 +1,20 @@
+package com.example.consumer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/consumer")
+public class ConsumerController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/test")
+    public String test(){
+        return restTemplate.getForEntity("http://localhost:8010/provider/test", String.class).getBody();
+    }
+}
