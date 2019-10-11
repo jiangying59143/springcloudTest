112d74be870fa5200802977f2344a7607bd9ab9a
jiangying59143 <907292671@qq.com>
2019-10-11 18:54:19 +0800

    native spring cloud config

---
+++ native-config-client/src/main/java/com/example/nativeconfigclient/NativeConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.nativeconfigclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NativeConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NativeConfigClientApplication.class, args);
+    }
+
+}
---
+++ native-config-client/src/main/java/com/example/nativeconfigclient/controller/ClientController.java
@@ -0,0 +1,22 @@
+package com.example.nativeconfigclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/config-client")
+public class ClientController {
+
+    @Value("${server.port}")
+    private String port;
+
+    @Value("${info}")
+    private String info;
+
+    @GetMapping("/test")
+    public String test(){
+        return port + "-" + info + 4;
+    }
+}
