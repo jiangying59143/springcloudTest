430b19861e241af06337676f8cb22f781d1d2ab9
jiangying59143 <907292671@qq.com>
2019-10-14 21:38:33 +0800

    zipkin

---
+++ zipkin-client/src/main/java/com/example/zipkinclient/controller/ClientController.java
@@ -0,0 +1,19 @@
+package com.example.zipkinclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/zipkin")
+public class ClientController {
+
+    @Value("${server.port}")
+    private String port;
+
+    @GetMapping("/test")
+    public String test(){
+        return port;
+    }
+}
