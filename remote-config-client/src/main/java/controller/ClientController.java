a57dc5d8c9090e7c12597af9dfa9411707cfd3dd
jiangying59143 <907292671@qq.com>
2019-10-14 19:11:10 +0800

    remote spring cloud config

--- remote-config-client/src/main/java/controller/ClientController.java
+++ remote-config-client/src/main/java/controller/ClientController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/config-client")
+@RequestMapping("/remote-config-client")
 public class ClientController {
 
     @Value("${server.port}")
