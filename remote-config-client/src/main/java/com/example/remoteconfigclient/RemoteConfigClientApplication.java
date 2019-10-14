8a445b167ea2b59ede30fc157b10dca8c88895d2
jiangying59143 <907292671@qq.com>
2019-10-14 19:01:02 +0800

    remote spring cloud config

---
+++ remote-config-client/src/main/java/com/example/remoteconfigclient/RemoteConfigClientApplication.java
@@ -0,0 +1,14 @@
+package com.example.remoteconfigclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemoteConfigClientApplication.class, args);
+    }
+
+}
